894734ea75249b13574ccb6e3f871fcec8e556ff
Johnston Stewart <bigjohnny@gmail.com>
2026-08-19 16:35:00 +0100

    dropdown menu rejigged,much cleaner, smooth animation

--- assets/menu-show.js
+++ assets/menu-show.js
@@ -1,15 +1,16 @@
 menuShowHideFunction();
 function menuShowHideFunction() {
-    var x = document.getElementById("drop-menu-ul");
+    var x = document.getElementById("drop-menu");
+    var xul = document.getElementById("drop-menu-ul");
     var y = document.getElementById("burger-open");
     var z = document.getElementById("burger-close");
-    if (x.style.display === "none") {
-      x.style.display = "inline-flex";
-      y.style.display = "none";
-      z.style.display = "flex";
-    } else {
-      x.style.display = "none";
+    if (!x.classList.contains("drop-menu-hidden")) {
+      x.classList.add("drop-menu-hidden");
       y.style.display = "flex";
       z.style.display = "none";
+    } else {
+      x.classList.remove("drop-menu-hidden");
+      y.style.display = "none";
+      z.style.display = "flex";
     }
   } 
